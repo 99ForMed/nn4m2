c2ebd614a1e9fcb22db962edcdafa47bcdf079cd
callmepri <140776053+callmepri@users.noreply.github.com>
2023-12-16 10:15:16 +1100

    asdf

--- nn4m/settings.py
+++ nn4m/settings.py
@@ -166,15 +166,14 @@ EMAIL_USE_SSL = True
 if True:
     # Use the storage backend for static and media files
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID="AKIA3SUPPFQ3KHX6LDOP"
-    AWS_SECRET_ACCESS_KEY="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"
-    AWS_STORAGE_BUCKET_NAME = '99formed-main'
-    AWS_S3_REGION_NAME = 'ap-southeast-2'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY=os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 
-    # Use the same storage backend for static files
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    # Static and media file URLs
+
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
