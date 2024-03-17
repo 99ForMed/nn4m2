3dad08994d2978f4c26ff765ec336da64b1efc1e
callmepri <140776053+callmepri@users.noreply.github.com>
2024-03-17 22:56:19 +1100

    dsfg

--- staticfiles/course-page-updated/js/playAnimations.0ca5e975cdd1.js
+++
@@ -1,9 +0,0 @@
-function playAnimation1() {
-    var video = document.getElementById('animation1Video');
-    video.play();
-}
-
-function playAnimation2() {
-    var video = document.getElementById('animation2Video');
-    video.play();
-}
